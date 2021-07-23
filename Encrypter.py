e59ff6149f93e2ed835b6447d381d8101dfda08d
Amitash <amitu895@gmail.com>
2021-07-23 21:14:01 +0530

    Performed Initial Permutation of the string to be encrypted

--- Encrypter.py
+++ Encrypter.py
@@ -39,7 +39,7 @@ def round_of_16(key_56):
 string_data64 = '0000 0001 0010 0011 0100 0101 0110 0111 1000 1001 1010 1011 1100 1101 1110 1111'
 string_data64 = string_data64.split()
 string_data64 = list(''.join(string_data64))
-data64 = [int(i) for i in string_data64]
+data_64 = [int(i) for i in string_data64]
 
 # key_64 = bitarray('rahul').get_bytes()
 string_64 = '0001001100110100010101110111100110011011101111001101111111110001' #key as 64 binary bits
@@ -65,7 +65,7 @@ key_56 = split_half(key_56)
 #key_56 is same as k0 =c0+d0
 
 keys = round_of_16(key_56) #16 subkeys are obtained
-print(keys)
+#print(keys)
 
 tmp_k = keys.copy()
 
@@ -75,7 +75,10 @@ pc2 = [14, 17, 11, 24, 1, 5, 3, 28, 15, 6, 21, 10, 23, 19, 12, 4, 26, 8, 16, 7, 
 for i in range(len(keys)):
     keys[i] = permutation(keys[i], pc2)
 
-print(keys)
+#print(keys)
 
 #IP
 ip = [58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44, 36, 28, 20, 12, 4, 62, 54, 46, 38, 30, 22, 14, 6, 64, 56, 48, 40, 32, 24, 16, 8, 57, 49, 41, 33, 25, 17, 9, 1, 59, 51, 43, 35, 27, 19, 11, 3, 61, 53, 45, 37, 29, 21, 13, 5, 63, 55, 47, 39, 31, 23, 15, 7]
+
+ipdata_64 = permutation(data_64, ip)
+print(ipdata_64)
