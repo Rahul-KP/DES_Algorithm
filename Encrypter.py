ace9ee3d60b95ee068f139eeb7f67a755e588171
Amitash <amitu895@gmail.com>
2021-07-26 08:43:19 +0530

    changed variable names and cleaned up code

--- Encrypter.py
+++ Encrypter.py
@@ -16,12 +16,12 @@ def lcs(tmp_list):
 	tmp_list.remove(tmp_list[0])
 	return tmp_list
 
-def round_of_16(key_56):
+def round_of_16(K_plus):
 
 	keys = [] #16 keys of 56bits each
 
-	c = key_56[0]# 28 bits
-	d = key_56[1]
+	c = K_plus[0]# 28 bits
+	d = K_plus[1]
 	one_lcs = [1,2,9,16]
 	for i in range(1,17):
 		if i in one_lcs:
@@ -39,35 +39,35 @@ def round_of_16(key_56):
 string_data64 = '0000 0001 0010 0011 0100 0101 0110 0111 1000 1001 1010 1011 1100 1101 1110 1111'
 string_data64 = string_data64.split()
 string_data64 = list(''.join(string_data64))
-data_64 = [int(i) for i in string_data64]
+M = [int(i) for i in string_data64]
 
-# key_64 = bitarray('rahul').get_bytes()
+# K = bitarray('rahul').get_bytes()
 string_64 = '0001001100110100010101110111100110011011101111001101111111110001' #key as 64 binary bits
-key_64 = list(string_64)
-key_64 = [int(i) for i in key_64] #converting the string of 64-bit key to a list of 64 bits of datatype 'int'
+K = list(string_64)
+K = [int(i) for i in K] #converting the string of 64-bit key to a list of 64 bits of datatype 'int'
 
 #Zero padding
-while(len(key_64) < 64):
+while(len(K) < 64):
 	i = 0
-	key_64.insert(i,0)
+	K.insert(i,0)
 	i += 1
 
 #PC-1
 pc1 = [57,49,41,33,25,17,9,1,58,50,42,34,26,18,10,2,59,51,43,35,27,19,11,3,60,52,44,36,63,55,47,39,31,23,15,7,62,54,46,38,30,22,14,6,61,53,45,37,29,21,13,5,28,20,12,4]
 
 #Applying PC-1
-# key_56 = [] #aka permuted key ,k+
+# K_plus = [] #aka permuted key ,k+
 
-key_56 = permutation(key_64, pc1)
+K_plus = permutation(K, pc1)
 
-key_56 = split_half(key_56)
-#print(key_56)
-#key_56 is same as k0 =c0+d0
+K_plus = split_half(K_plus)
+#print(K_plus)
+#K_plus is same as k0 =c0+d0
 
-keys = round_of_16(key_56) #16 subkeys are obtained
+keys = round_of_16(K_plus) #16 subkeys are obtained
 #print(keys)
 
-tmp_k = keys.copy()
+#tmp_k = keys.copy()
 
 #PC2
 pc2 = [14, 17, 11, 24, 1, 5, 3, 28, 15, 6, 21, 10, 23, 19, 12, 4, 26, 8, 16, 7, 27, 20, 13, 2, 41, 52, 31, 37, 47, 55, 30, 40, 51, 45, 33, 48, 44, 49, 39, 56, 34, 53, 46, 42, 50, 36, 29, 32]
@@ -77,8 +77,11 @@ for i in range(len(keys)):
 
 #print(keys)
 
-#IP
+#ip - data for permutation(table)
 ip = [58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44, 36, 28, 20, 12, 4, 62, 54, 46, 38, 30, 22, 14, 6, 64, 56, 48, 40, 32, 24, 16, 8, 57, 49, 41, 33, 25, 17, 9, 1, 59, 51, 43, 35, 27, 19, 11, 3, 61, 53, 45, 37, 29, 21, 13, 5, 63, 55, 47, 39, 31, 23, 15, 7]
 
-ipdata_64 = split_half(permutation(data_64, ip))
-print(ipdata_64)
+#IP - message bits after initial permutation (ip)
+IP = permutation(M, ip)
+#print(IP)
+temp = [IP[i:i+4] for i in range(0, len(IP), 4)]
+print(temp)
